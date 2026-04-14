9ca4c12e3df44726c0fc7bfb19246648f50df925
Manish Muhal <manishmuhal89@gmail.com>
2026-04-14 14:41:01 +0530

    bug

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    traceIgnore: [
+      "node_modules/@fontsource/**",
+      "node_modules/animate.css/**",
+      "node_modules/swiper/**",
+      "node_modules/gsap/**",
+      "node_modules/@fancyapps/**",
+      "node_modules/isotope-layout/**",
+      "node_modules/react-icons/**",
+      "node_modules/react-multi-carousel/**",
+      "node_modules/yet-another-react-lightbox/**",
+    ],
+  },
 };
 
 export default nextConfig;
