b5df86763947df65da685bca4ac3f1c8b74a0720
Manish Muhal <manishmuhal89@gmail.com>
2026-04-14 14:46:57 +0530

    ss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   typescript: {
     ignoreBuildErrors: false,
   },
@@ -10,19 +9,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    traceIgnore: [
-      "node_modules/@fontsource/**",
-      "node_modules/animate.css/**",
-      "node_modules/swiper/**",
-      "node_modules/gsap/**",
-      "node_modules/@fancyapps/**",
-      "node_modules/isotope-layout/**",
-      "node_modules/react-icons/**",
-      "node_modules/react-multi-carousel/**",
-      "node_modules/yet-another-react-lightbox/**",
-    ],
   },
 };
 
